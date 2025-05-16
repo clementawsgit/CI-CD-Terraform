c7196f871f5ca5f1c3b7b6497db28265ee6cdb01
clement <jeromeit05@gmail.com>
2025-05-16 16:25:52 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,63 +1,46 @@
 terraform {
   required_providers {
     aws = {
-        source = "hashicorp/aws"
-        version = "~>5.0"
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
     }
   }
 }
 
-#defining aws provider
-provider aws {
-    region = "us-east-1"
-    access_key = "AKIAWCZC5ZCVTGSUQC2E"
-    secret_key = "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"
+provider "aws" {
+  region = var.aws_region
+  # In a real CI/CD pipeline, AWS credentials would be managed
+  # through IAM roles assigned to the CodePipeline service role.
+  # Avoid hardcoding credentials here or in tfvars for production.
 }
 
-#defining ec2 machine
-resource aws_instance "myec" {
-    ami = "ami-0c02fb55956c7d316"
-    instance_type = "t2.micro"
-    key_name = "linux"
-    tags = {
-        Name = "clement-vm"
-    }
+resource "aws_instance" "cicd_ec2" {
+  ami           = var.ami_id
+  instance_type = var.instance_type
+  key_name      = var.key_pair_name
+  subnet_id     = var.subnet_id
+  vpc_security_group_ids = [aws_security_group.allow_ssh.id]
+  tags = merge(var.ec2_tags, { Name = var.instance_name })
 }
 
-#defining vpc
-resource aws_vpc "myvpc" {
-    cidr_block = "10.0.0.0/16"
-    tags = {
-        Name = "clementvpc"
-    }
-}
+resource "aws_security_group" "allow_ssh" {
+  name_prefix = "cicd-ssh-"
+  description = "Allow SSH inbound traffic"
+  vpc_id      = var.vpc_id
 
-#defining s3 bucket
-resource aws_s3_bucket "mys3bucket" {
-    bucket = "singapore1405bkt"
-}
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = var.ssh_cidr_blocks
+  }
 
-#defining security group
-resource aws_security_group "allow_ssh" {
-    name = "allow_ssh"
-    description = "allows incoming ssh traffic"
-    vpc_id = "vpc-083bd5085ee1b2149"
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-    ingress {
-        from_port = "22"
-        to_port = "22"
-        protocol = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
-
-    egress {
-        from_port = "0"
-        to_port = "0"
-        protocol = "-1"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
-
-    tags = {
-        Name = "clem-ssh-sg"
-    }
+  tags = var.sg_tags
 }
